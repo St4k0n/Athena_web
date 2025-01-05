15149e0db22eca65e2acf422c01aa2cd0d3e8eeb
Astramat <matthis.brocheton@epitech.eu>
2025-01-05 14:51:12 +0800

    Edit ( ✨ ) edit basePath for page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,12 @@ import getConfig from "next/config";
 import Image from "next/image";
 const { publicRuntimeConfig } = getConfig();
 
-const basePath = publicRuntimeConfig.basePath;
+
 
 export default function Home() {
+  
+  const basePath = publicRuntimeConfig.basePath;
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
